0a14b5fb24f584e364f97838b075ad4de0205f49
isaydiev <isaydiev86@gmail.com>
2023-10-06 13:01:13 +0500

    add concurrency healths_service

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/Shemistan/healths_service/config"
 )
 
 func main() {
+	log.Println("start monitoring service")
 	// init config
 	conf, err := config.NewConfig()
 	if err != nil {
@@ -24,21 +26,75 @@ func main() {
 		log.Fatal("failed get urls", err)
 	}
 
-	ticker := time.NewTicker(conf.Delay)
+	// Указываем количество горутин
+	goroutineCount := conf.GoroutineCount
 
-	for range ticker.C {
+	requests := make(chan string, len(apiList))
+	//done := make(chan bool)
+	wg := sync.WaitGroup{}
+
+	go func() {
 		for _, api := range apiList {
-			resp, err := http.Get(api)
-			if err != nil || resp.StatusCode != http.StatusOK {
-				fmt.Printf("Ошибка мониторинга сервиса %s\n", api)
-			} else {
-				fmt.Printf("Сервис %s работает исправно\n", api)
+			requests <- api
+		}
+	}()
+
+	defer close(requests)
+
+	wg.Add(goroutineCount)
+
+	for i := 0; i < goroutineCount; i++ {
+		go func() {
+			defer wg.Done()
+			for api := range requests {
+				resp, err := http.Get(api)
+				if err != nil || resp.StatusCode != http.StatusOK {
+					log.Printf("Ошибка мониторинга сервиса %s\n", api)
+				} else {
+					log.Printf("Сервис %s работает исправно\n", api)
+				}
+
+				if resp != nil {
+					resp.Body.Close()
+				}
 			}
-			if resp != nil {
-				resp.Body.Close()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		//done <- true
+	}()
+
+	ticker := time.NewTicker(conf.Delay)
+	for {
+		select {
+		//case <-time.After(conf.Delay):
+		//	log.Println("wait run after delay monitoring service")
+		//time.Sleep(conf.Delay)
+		case <-ticker.C:
+			for _, api := range apiList {
+				requests <- api
 			}
+			//default:
+			//	time.After(conf.Delay)
+			//	log.Println("wait run after delay monitoring service")
 		}
 	}
+
+	//for range ticker.C {
+	//	for _, api := range apiList {
+	//		resp, err := http.Get(api)
+	//		if err != nil || resp.StatusCode != http.StatusOK {
+	//			fmt.Printf("Ошибка мониторинга сервиса %s\n", api)
+	//		} else {
+	//			fmt.Printf("Сервис %s работает исправно\n", api)
+	//		}
+	//		if resp != nil {
+	//			resp.Body.Close()
+	//		}
+	//	}
+	//}
 
 }
 
@@ -50,7 +106,7 @@ func getApiUrl() ([]string, error) {
 	file, err := os.Open("config/api_list.txt")
 	if err != nil {
 		fmt.Println("Ошибка открытия файла:", err)
-		return nil, nil
+		return nil, err
 	}
 	defer file.Close()
 
--- internal/models/config.go
+++ internal/models/config.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type Config struct {
-	App   APP           `envconfig:"APP"`
-	Delay time.Duration `envconfig:"DELAY"`
+	App            APP           `envconfig:"APP"`
+	Delay          time.Duration `envconfig:"DELAY"`
+	GoroutineCount int           `envconfig:"GOROUTINE_COUNT"`
 }
 
 type APP struct {
