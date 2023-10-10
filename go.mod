124c045996aa041d8707cbe69a062750ad1ff09c
ILLHOME <yastreb557@gmail.com>
2023-10-10 19:02:47 +0500

    That's all I managed to do

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Shemistan/healths_service
 
-go 1.21.0
+go 1.23
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ internal/bot.go
@@ -0,0 +1,88 @@
+package internal
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Bot struct {
+	bot          *tgbotapi.BotAPI
+	updateConfig tgbotapi.UpdateConfig
+	data         map[int64]*Runner
+	fileName     string
+}
+
+func (b *Bot) Init(fileName string) {
+	b.fileName = fileName
+	buff, _ := os.ReadFile(fileName)
+	rawData := strings.Split(string(buff), "\n")
+	data := make(map[int64][]string)
+
+	b.bot, _ = tgbotapi.NewBotAPI(rawData[0])
+	b.data = make(map[int64]*Runner)
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+
+	if len(rawData) > 1 {
+		for i := 1; i < len(rawData); i++ {
+			arr := strings.Split(rawData[i], " ")
+			chatID, _ := strconv.ParseInt(arr[1], 10, 64)
+			data[chatID] = append(data[chatID], arr[2])
+		}
+
+		for chatID := range data {
+			b.data[chatID] = NewRunner(false)
+			b.data[chatID].Add(data[chatID])
+		}
+	}
+}
+
+func (b *Bot) Send(msg string, chatID int64) error {
+	_, err := b.bot.Send(tgbotapi.NewMessage(chatID, msg))
+	return err
+}
+
+func (b *Bot) Receive() {
+	updates, _ := b.bot.GetUpdatesChan(b.updateConfig)
+	result := make(chan string)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "add":
+				if b.data[update.Message.Chat.ID] == nil {
+					b.data[update.Message.Chat.ID] = NewRunner(false)
+				}
+				b.data[update.Message.Chat.ID].Add([]string{strings.ReplaceAll(update.Message.Text, "/add ", "")})
+			case "remove":
+				b.data[update.Message.Chat.ID].Remove(strings.ReplaceAll(update.Message.Text, "/remove ", ""))
+			case "run":
+				{
+					go b.data[update.Message.Chat.ID].Start(result, 10)
+					go b.Send(<-result, update.Message.Chat.ID)
+				}
+			case "stop":
+				b.data[update.Message.Chat.ID].Stop()
+			}
+		} else {
+			b.Send("Use commands to operate with the bot", update.Message.Chat.ID)
+		}
+	}
+}
+
+func (b *Bot) Stop() {
+	b.bot.StopReceivingUpdates()
+	for chatID := range b.data {
+		b.data[chatID].Stop()
+		for _, url := range b.data[chatID].Export() {
+			os.WriteFile(b.fileName, []byte(strconv.FormatInt(chatID, 10)+" "+url+"\n"), 0644)
+		}
+	}
+}
